5b3f49e6b5de76362c91f4f4c4bf2ce2322e0fec
lazyfetch <ne0fetc4@gmail.com>
2025-08-24 15:01:07 +0300

    ++

---
+++ crates/mc-client/src/config.rs
@@ -0,0 +1,5 @@
+pub const DEFAULT_SERVER_HOST: &str = "localhost";
+pub const DEFAULT_SERVER_PORT: u16 = 25565;
+pub const DEFAULT_USERNAME: &str = "mine_rs";
+pub const DEFAULT_COMPRESSION_THRESHOLD: i32 = -1;
+pub const PROTOCOL_VERSION_1_21_8: i32 = 772;
